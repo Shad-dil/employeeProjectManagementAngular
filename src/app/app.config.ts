56fe69503da301b90f89232bfe0b8cbd7a3cd986
Dilshad <helloshad3@gmail.com>
2025-01-02 21:27:20 +0530

    Login Component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
 
+import { provideHttpClient } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+  ],
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ToastrModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-100%)' }),
+        animate(300),
+      ]),
+      transition(':leave', [
+        animate(300, style({ opacity: 0, transform: 'translateY(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class LoginComponent {
+  loginObj: any = {
+    username: '',
+    password: '',
+  };
 
+  http = inject(HttpClient);
+  router = inject(Router);
+  toast: ToastrService;
+  constructor(toast: ToastrService) {
+    this.toast = toast;
+  }
+  onLogin(form: any) {
+    this.http
+      .post(
+        'https://projectapi.gerasim.in/api/EmployeeManagement/login',
+        this.loginObj
+      )
+      .subscribe((res: any) => {
+        if (res.result) {
+          localStorage.setItem('employeeApp', JSON.stringify(res.data));
+          this.router.navigateByUrl('/dashboard');
+          this.toast.success('Login Successful');
+        } else {
+          this.toast.error('Invalid Username or Password');
+        }
+      });
+    form.reset();
+  }
 }
